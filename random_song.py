7c2f0c2d20c04e19c1457625b57e3f5fe6eef51c
Nofar Fins <nofarfins11@gmail.com>
2022-05-04 17:42:55 +0300

    initial project commit

---
+++ random_song.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+# from flask_cors import CORS
+#
+# app = Flask(__name__)
+# cors = CORS(app, resources={"/api"})
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/topten', methods=['GET'])
+def ReturnJSON():
+    if (request.method == 'GET'):
+        data ={
+            "name": ['Pepas - Farrukou' ,
+                     'Essence - WizKid ft. Justin Bieber and Tems',
+                     'All Too Well - Taylor Swift',
+                     'VBS- Lucy Dacus',
+                     'Monster - Yoasobi',
+                    'Twerkulator - City Girls',
+                     'Good 4 U - Olivia Rodrigo',
+                     'La Mama de La Mama - El Alfa',
+                    'Wilder Days - Morgan Wade',
+                    'Woman - Doja Cat' ]}
+        return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
